839d32c1fc1d5c701dc3bdccd8aa3acf391a2992
Anders Abel <anders.abel@sustainsys.com>
2020-02-14 09:31:06 +0100

    Add Owin startup

---
+++ MvcOwinApp/App_Start/Startup.Auth.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOwinApp
+{
+    public partial class Startup
+    {
+        private void ConfigureAuth(IAppBuilder app)
+        {
+
+        }
+    }
+}
